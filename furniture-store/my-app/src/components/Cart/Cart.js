65442d506682868d97b7beb81ab4bacd239e9103
TangiresH <dimon4ik1010@gmail.com>
2024-06-21 02:14:00 +0300

    added notification upon adding item to cart and implemented cart button

--- furniture-store/my-app/src/components/Cart/Cart.js
+++ furniture-store/my-app/src/components/Cart/Cart.js
@@ -1,22 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import CartContext from '../CartContext/CartContext';
 import './Cart.css';
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
+  const [isCartVisible, setIsCartVisible] = useState(false);
+  const [cartCount, setCartCount] = useState(0);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    let totalCount = cart.reduce((total, item) => total + item.quantity, 0);
+    setCartCount(totalCount);
+
+    if (cart.length > 0) {
+      setShowNotification(true);
+      const timer = setTimeout(() => {
+        setShowNotification(false);
+      }, 800);
+
+      return () => clearTimeout(timer);
+    }
+  }, [cart]);
+
+  const toggleCartVisibility = () => {
+    setIsCartVisible(!isCartVisible);
+  };
 
   return (
-    <div className="cart">
-      <h2>Кошик</h2>
-      <ul>
-        {cart.map((item, index) => (
-          <li key={index}>
-            <span>{item.name}</span>
-            <span>{item.price}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <button className="cart-button" onClick={toggleCartVisibility}>
+        <img src="cart-logo.png" alt="Cart" className="cart-logo" />
+        {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+      </button>
+      {isCartVisible && (
+        <div className="cart">
+          <button className="close-button" onClick={toggleCartVisibility}>
+            &times;
+          </button>
+          <h2>Кошик</h2>
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index}>
+                <span>{item.name}</span>
+                <span>{item.price}</span>
+                <span>Кількість: {item.quantity}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {showNotification && (
+        <div className="notification active">
+          Товар додано до кошика!
+        </div>
+      )}
+    </>
   );
 };
 
